b828fce139518bb173aeb6fbbf8293f660cc7d13
kiran <kiranjha.spearmint@gmail.com>
2023-03-21 14:47:47 +0530

    first commit

--- scripts/deploy.js
+++
@@ -1,5 +0,0 @@
-const { ethers } = require('hardhat');
-
-async () => {
-    const [owner] = await ethers.getSigners();
-}
--- scripts/metadata.js
+++ scripts/metadata.js
@@ -1,9 +1,9 @@
-var fs = require('fs');
-for (var i = 1; i<=4; i++ ) {
-    var json = {}
-    json.name = "Token #" + i;
-    json.description = "This is the description for token #" + i;
-    json.image = "ipfs://bafybeie4x4fkepc3km4osop427lesjb5klp7hxxlauu33dflrgo4s3tuci" + i + ".jpg";
+// var fs = require('fs');
+// for (var i = 1; i<=4; i++ ) {
+//     var json = {}
+//     json.name = "Token #" + i;
+//     json.description = "This is the description for token #" + i;
+//     json.image = "ipfs://bafybeie4x4fkepc3km4osop427lesjb5klp7hxxlauu33dflrgo4s3tuci" + i + ".jpg";
 
-    fs.writeFileSync('' + i, JSON.stringify(json));
-}
+//     fs.writeFileSync('' + i, JSON.stringify(json));
+// }
